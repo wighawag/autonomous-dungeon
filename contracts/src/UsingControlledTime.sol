08efcc200b49a2eb0d990b5559864b6d16fedf37
wighawag <wighawag@gmail.com>
2023-05-22 22:01:47 +0100

    back to admin

--- contracts/src/UsingControlledTime.sol
+++ contracts/src/UsingControlledTime.sol
@@ -15,7 +15,7 @@ abstract contract UsingControlledTime is UsingInternalTimestamp {
         assembly {
             adminAddress := sload(0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103)
         }
-        // require(msg.sender == adminAddress, "NOT_ADMIN");
+        require(msg.sender == adminAddress, "NOT_ADMIN");
         uint256 newDelta = _delta() + delta;
         assembly {
             sstore(0x112c413de07a110ce0a9ace0c01e41b5b59462770325b042f0dc72c337f55f2, newDelta)
